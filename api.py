458bc929f96d0978f32db981748f3acc4bda77c4
cyrus-trixie <125303529+cyrus-trixie@users.noreply.github.com>
2025-04-29 15:05:27 +0300

    port

--- api.py
+++ api.py
@@ -250,4 +250,4 @@ def serve_react(path):
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
